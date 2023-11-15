d9aa1210779f86a74de4d62a2baaa2235c87d1e2
ziosss <93509214+zios0707@users.noreply.github.com>
2023-11-15 19:03:41 +0900

    [level unrated] Title: ad 제거하기, Time: 3.59 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,7 @@
+import java.util.*;              
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        return Arrays.stream(strArr).filter(s -> !s.contains("ad")).toArray(String[]::new);
+    }
+}
