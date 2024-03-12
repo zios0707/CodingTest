d029b9378604e9c0553eb2149af7e2c56935ba0e
강현욱 <93509214+zios0707@users.noreply.github.com>
2024-03-12 19:58:48 +0900

    [Bronze V] Title: A+B - 4, Time: 232 ms, Memory: 17904 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt(), m = sc.nextInt();
+            System.out.println(n + m);
+        }
+    }
+}
