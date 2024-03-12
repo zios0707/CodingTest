9792977cf46dd8570abd96949d78c3828b0f4d9b
강현욱 <93509214+zios0707@users.noreply.github.com>
2024-03-12 19:45:44 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17860 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int o = sc.nextInt();
+            for (int i = 0; i < o; i++) {
+                int n = sc.nextInt(), m = sc.nextInt();
+                System.out.println(n + m);
+            }
+        }
+}
