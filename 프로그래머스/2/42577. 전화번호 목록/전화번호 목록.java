0ce208c09139eb7587312cabb2c7bc5072a9f406
강현욱 <93509214+zios0707@users.noreply.github.com>
2024-04-09 14:30:50 +0900

    [level 2] Title: 전화번호 목록, Time: 548.42 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
+import java.util.Comparator;
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+        phone_book = Arrays.stream(phone_book).sorted().toArray(String[]::new);
         boolean tkn = true;
         for (int i = 0; i < phone_book.length - 1; i++)
-                if (phone_book[i + 1].startsWith(phone_book[i]))
-                    tkn = false;
+            if (phone_book[i + 1].startsWith(phone_book[i]))
+                tkn = false;
         return tkn;
     }
 }
