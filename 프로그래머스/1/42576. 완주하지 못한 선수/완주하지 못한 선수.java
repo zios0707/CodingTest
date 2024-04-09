57096a9665f04a8639bae58613d2f4028696abb0
강현욱 <93509214+zios0707@users.noreply.github.com>
2024-04-09 14:10:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.61 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,7 +3,9 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         HashMap<String, Integer> map = new HashMap<>();
         for (String s : participant) map.put(s, map.getOrDefault(s, 0) + 1);
-        for (String s : completion) map.put(s, map.getOrDefault(s, 0) - 1);
+        for (String s : completion)
+            if (map.get(s) <= 0) map.remove(s);
+            else map.put(s, map.getOrDefault(s, 0) - 1);
         for (String s : map.keySet()) if (map.get(s) == 1) return s;
         return null; // 컴파일러 오작동해서 이렇게함
     }
