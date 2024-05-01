0a09ffe04aa0f0893091588b950ae562b4b7c76e
강현욱 <93509214+zios0707@users.noreply.github.com>
2024-05-01 19:19:14 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 13.29 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/README.md
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 쿼드압축 후 개수 세기 - 68936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936#) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 19.86 ms
+메모리: 106 MB, 시간: 13.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 01일 18:51:34
+2024년 05월 01일 19:19:12
 
 ### 문제 설명
 
--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,17 +1,14 @@
 class Solution {
     int zero;
     int one;
-    int n;
     int[][] array;
 
 
     public int[] solution(int[][] arr) {
         zero = 0;
         one = 0;
-        n = 0;
         array = arr;
-        n = getSquare(arr.length);
-        int val = cal(0, 0, n);
+        int val = cal(0, 0, arr.length);
         if(val == 1) one++;
         else if(val == 0) zero++;
 
@@ -20,24 +17,28 @@ class Solution {
     }
 
     // returns : -1, 0, 1
-    // -1 은 값이 복잡하다는 뜻. 1은 truthy, 0은 falsy 로도 해석
+    // -1 은 값이 복잡하다는 뜻. 1은 truthy, 0은 falsy 로도 해석.
+    // base X Y 좌표로 시작해서 N / 2 칸 간격으로 전부 검사.
+    // 
     private int cal(int x, int y, int N) {
-        if(N == 0) return (array[x][y] == 1) ? 1 : 0;
-        int radix = getNumberBySquare(N - 1);
+        if(N == 1) return (array[x][y] == 1) ? 1 : 0;
+        int radix = N / 2;
         int[] radixX = new int[]{x, x + radix, x, x + radix};
         int[] radixY = new int[]{y, y, y + radix, y + radix};
+        // BFS 같은 근접 탐색할 때 자주 쓰이는 기법. 자주 쓰임.
+        
         int isEquals = 0;
         int[] saveList = new int[4];
 
-        for(int i = 0; i < 4; i++) {
-            int val = cal(radixX[i], radixY[i], N - 1);
+        for(int i = 0; i < 4; i++) { // 4분할 이기에 탐색할 곳은 4곳
+            int val = cal(radixX[i], radixY[i], N / 2);
             saveList[i] = val;
             isEquals += (val != -1) ? (val == 1) ? 1 : -1 : 0;
-            // 0 인 경우 내부에서 이탈이 있다는 뜻 -> 절대 4, -4가 되지 못함
+            // 반환 0 인 경우 내부에서 이탈이 있다는 뜻 -> 절대 4, -4가 되지 못함
             // 서로 다른 수가 영역 내에서 블럭씩으로 묶어진다해도 1, -1이 있어 4, -4가 되지 못함
         }
 
-        if(isEquals % 4 == 0 && isEquals != 0) { // n ^ N 크기의 블럭이 하나로 묶을 수 있는 상태
+        if(isEquals / 4 != 0) { // n ^ N 크기의 블럭이 하나로 묶을 수 있는 상태
             return (isEquals == 4) ? 1 : 0;
         }else {
             for(int i: saveList) {
@@ -46,17 +47,5 @@ class Solution {
             }
             return -1;
         }
-    }
-
-    private int getNumberBySquare(int N) {
-        int i = 1;
-        for(int j = 1; j <= N; j++) i *= 2;
-        return i;
-    }
-
-    private int getSquare(int m) {
-        int N = 0;
-        for(int i = 1; i < m; i *= 2) N += 1;
-        return N;
     }
 }
