f6cd3149ff3dc7f4171291e3dbdc27ffb63fef1e
강현욱 <93509214+zios0707@users.noreply.github.com>
2024-10-23 19:41:35 +0900

    [Bronze III] Title: 연세여 사랑한다, Time: 172 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/25915. 연세여 사랑한다/연세여 사랑한다.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Math.abs(sc.next().charAt(0) - 'I') + 84);
+    }
+}
